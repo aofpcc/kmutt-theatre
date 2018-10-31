a260b876619f318d5f904b142898572740dc95a7
tangpnm <tang_6528@hotmail.com>
2018-10-31 14:30:40 +0700

    query price from ticket
    ....

--- groups/group1/file1.php
+++ groups/group1/file1.php
@@ -1,58 +1,10 @@
-<? session_start(); ?>
-
-<?php
-require_once(''); ?>
-
-<?php
-require_once(''); ?>
-<?php
-
-$chair = array('' );
-$price =
-
- ?>
-
-
-
 <?php
 $klein->respond('GET', '/group1', function ($request, $response, $service) {
   global $database;
   $conn = $database->getConnection();
 
-  $query = "SELECT * from movies";
+  $query = "SELECT price from ticket";
   $stmt = $conn->prepare($query);
-  $stmt->execute();
 
-  $num = $stmt->rowCount();
-  $arr = $stmt->fetchAll(PDO::FETCH_BOTH);
-
-  $service->allMovies = $arr;
-  $service->pageTitle = 'Hello';
-  $service->render('layouts/group1/home.php');
-});
-
-$klein->respond('GET', '/group', function ($request, $response, $service) {
-  global $database;
-  $conn = $database->getConnection();
-
-  $query = "SELECT * from movies";
-  $stmt = $conn->prepare($query);
-  $stmt->execute();
-  $num = $stmt->rowCount();
-
-  $result = array();
-
-  if($num > 0) {
-    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-      extract($row);
-      array_push($result, array(
-        "id" => $id,
-        "name" => $name
-      ));
-    }
-  }
-
-  $service->pageTitle = 'Hello';
-  $service->allMovies = $result;
-  $service->render('layouts/group1/bookingPage.php');
-});
+  
+ ?>

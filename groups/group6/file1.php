72b670da0bed878d4a574ff7de4fd9c42c30d20a
Maxine <night1714@hotmail.com>
2018-11-10 21:54:58 +0700

    Put php

--- groups/group6/file1.php
+++ groups/group6/file1.php
@@ -3,14 +3,13 @@ $klein->respond('GET', '/group6', function ($request, $response, $service) {
   global $database;
   $conn = $database->getConnection();
 
-  $query = "SELECT * from movies";
+  $query = "SELECT name from movies";
   $stmt = $conn->prepare($query);
   $stmt->execute();
 
   $num = $stmt->rowCount();
   $arr = $stmt->fetchAll(PDO::FETCH_BOTH);
 
-  $service->allMovies = $arr;
-  $service->pageTitle = 'Hello';
-  $service->render('layouts/group6/home.php');
+  echo json_encode($arr);
 });
+?>

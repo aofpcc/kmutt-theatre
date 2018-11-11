8c67b9d7f6767cac2682ed25845d5a8eb5022f1f
Maxine <night1714@hotmail.com>
2018-11-12 04:51:14 +0700

    Register system

--- groups/group6/file1.php
+++ groups/group6/file1.php
@@ -3,7 +3,7 @@ $klein->respond('GET', '/group6', function ($request, $response, $service) {
   global $database;
   $conn = $database->getConnection();
 
-  $query = "SELECT name from movies";
+  $query = "SELECT FirstName from Membership";
   $stmt = $conn->prepare($query);
   $stmt->execute();
 
@@ -31,10 +31,37 @@ $klein->respond('GET', '/group6/loginn', function ($request, $response, $service
   echo json_encode($arr);
 });
 
+$klein->respond('GET', '/group6/regis', function ($request, $response, $service) {
+  global $database;
+  $conn = $database->getConnection();
 
-$klein->respond('GET', '/group6/max', function($request, $response, $service ) {
+  $user = $_GET['user'];  $pass = $_GET['pass'];  $firstname = $_GET['firstname'];
+  $lastname = $_GET['lastname'];  $gender = $_GET['gender'];  $birthdate = $_GET['birthdate'];
+  $email = $_GET['email'];  $phonenumber = $_GET['phonenumber'];  $address = $_GET['address'];
+  $district = $_GET['district'];  $province = $_GET['province'];  $postcode = $_GET['postcode'];
 
-  $response->body("Max");
+  $query = "INSERT INTO Membership (Username, Password, FirstName,LastName,Gender,Birthdate,Email,PhoneNumber,Address,District,Province,Postcode)
+                          VALUES ('$user', '$pass', '$firstname', '$lastname', '$gender', '$birthdate', '$email', '$phonenumber', '$address', '$district', '$province', '$postcode')";
+  $stmt = $conn->prepare($query);
+  $stmt->execute();
+
+  $num = $stmt->rowCount();
+  $arr = $stmt->fetchAll(PDO::FETCH_BOTH);
+
+});
+
+$klein->respond('GET', '/group6/max', function ($request, $response, $service) {
+  global $database;
+  $conn = $database->getConnection();
+
+  $query = "SELECT ID from Membership";
+  $stmt = $conn->prepare($query);
+  $stmt->execute();
+
+  $num = $stmt->rowCount();
+  $arr = $stmt->fetchAll(PDO::FETCH_BOTH);
+
+  echo json_encode($arr);
 });
 
 $klein->respond('POST', '/group6/login', function ($request, $response, $service) {

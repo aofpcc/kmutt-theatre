fb7cf2b9aacefcce30552ab698284d9b03b9f667
Maxine <night1714@hotmail.com>
2018-11-11 17:34:36 +0700

    post json request login

--- groups/group6/file1.php
+++ groups/group6/file1.php
@@ -14,5 +14,25 @@ $klein->respond('GET', '/group6', function ($request, $response, $service) {
 });
 
 $klein->respond('GET', '/group6/max', function($request, $response, $service ) {
+
   $response->body("Max");
+});
+
+$klein->respond('GET', '/group6/login', function ($request, $response, $service) {
+  global $database;
+  $conn = $database->getConnection();
+
+  // $user = $request->user;
+
+  $query = "SELECT Username, Password from Membership where Username='Ay' and password='123s456'";
+  $stmt = $conn->prepare($query);
+  $stmt->execute();
+
+  $num = $stmt->rowCount();
+  $arr = $stmt->fetchAll(PDO::FETCH_BOTH);
+
+  if ($num == 1)
+    echo json_encode($arr);
+  else
+    echo 'error';
 });

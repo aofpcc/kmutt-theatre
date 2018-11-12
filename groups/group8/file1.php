013378edcc7447f30d23c9f3936e5079b931ede4
Dhanyasit <dhanyasit.46858@mail.kmutt.ac.th>
2018-11-12 11:13:47 +0700

    Promo code generater

--- groups/group8/file1.php
+++ groups/group8/file1.php
@@ -6,7 +6,7 @@ $klein->respond('GET', '/group8', function ($request, $response, $service) {
   $query = "SELECT * from movies";
   $stmt = $conn->prepare($query);
   $stmt->execute();
-
+ echo yamaha;
   $num = $stmt->rowCount();
   $arr = $stmt->fetchAll(PDO::FETCH_BOTH);
 

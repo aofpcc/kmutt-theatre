595f1c4ccda0f0b4c668b4acc828e92ad5b19ec9
Kornkanok Sangwichien <mu.nsnun@gmail.com>
2018-11-17 20:47:53 +0700

    [Financial] Update fronted

--- layouts/group11/component/statistics.php
+++ layouts/group11/component/statistics.php
@@ -1,6 +1,6 @@
 <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
   <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-    <h1 class="h2">Statistics</h1>
+    <h1 class="h2"><br>Statistics</h1>
   </div>
   <div style="margin-center:0 auto"></div>
   <!-- Code finance here -->
@@ -9,7 +9,7 @@
   <?php include('layouts/group11/component/layout/buttons.php'); ?>
   <!-- Button ends -->
 
-  
+
 
 <!-- Stop here -->
 </main>

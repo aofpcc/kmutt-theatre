09bd4cb60bf609bce8710fb9512c0efcc54dee45
Patipol Saechan <patipolsaechan@gmail.com>
2018-11-17 13:48:28 +0700

    [Update] Query statement

--- groups/group3/file1.php
+++ groups/group3/file1.php
@@ -3,13 +3,19 @@ $klein->respond('GET', '/group3', function ($request, $response, $service) {
   global $database;
   $conn = $database->getConnection();
 
-  $list = $conn->query("SELECT transactionId, dName, date, e.FirstName as empFN, e.LastName as empLN, m.FirstName as memFN, m.LastName as memLN, amount FROM Financial, FinancialID, Membership as m, employee as e WHERE Financial.FinID = FinancialID.ID AND  Financial.empID = e. EmpID AND Financial.customerID = m.ID")->fetchAll(PDO::FETCH_BOTH);
-  // $num = $stmt->rowCount();
-  // $arr = $stmt->fetchAll(PDO::FETCH_BOTH);
-
-  // $service->allMovies = $arr;
+  $revenueList = $conn->query(" SELECT transactionId, dName, date, e.FirstName as empFN, e.LastName as empLN, m.FirstName as memFN, m.LastName as memLN, amount
+                                FROM Revenue, FinancialID, Membership as m, employee as e
+                                WHERE Revenue.FinID = FinancialID.ID AND  Revenue.empID = e. EmpID AND Revenue.customerID = m.ID")->fetchAll(PDO::FETCH_BOTH);
   $service->pageTitle = 'Financial';
-  $service->list = $list;
+  $service->revenuelist = $revenueList;
+
+  $expensesList = $conn->query(" SELECT transactionId, dName, date, e.FirstName as empFN, e.LastName as empLN, m.FirstName as memFN, m.LastName as memLN, amount
+                                FROM Expenses, FinancialID, Membership as m, employee as e
+                                WHERE Expenses.FinID = FinancialID.ID AND  Expenses.empID = e. EmpID AND Expenses.customerID = m.ID")->fetchAll(PDO::FETCH_BOTH);
+  $service->pageTitle = 'Financial';
+  $service->expenseslist = $expensesList;
+
+  
   $service->render('layouts/group3/home.php');
   $conn = null;
 });

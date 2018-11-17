bb754590e296326499028ad273e5b6f2528e4f4e
Patipol Saechan <patipolsaechan@gmail.com>
2018-11-17 21:16:10 +0700

    [Update] Revenue
    finish add data to table

--- groups/group11/file1.php
+++ groups/group11/file1.php
@@ -168,10 +168,24 @@ $klein->respond('GET', '/staff/employee/dashboard', function ($request, $respons
     });
 
     $klein->respond('GET', '/staff/employee/revenue', function ($request, $response, $service) {
-
+        global $database;
+        $conn = $database->getConnection();
         $service->nameTag = 'revenue.php';
-        $service->render('layouts/group11/employee.php');
+
         echo($service->nameTag);
+
+        $list = $conn->query("SELECT dName , sum(amount) as total FROM FinancialID,Revenue
+                              WHERE Revenue.FinID = FinancialID.id
+                              GROUP BY dName")
+                              ->fetchAll(PDO::FETCH_BOTH);
+        $service->list = $list;
+
+        $revenueList = $conn->query(" SELECT transactionId, dName, date, e.FirstName as empFN, e.LastName as empLN, m.FirstName as memFN, m.LastName as memLN, amount
+                                      FROM Revenue, FinancialID, Membership as m, employee as e
+                                      WHERE Revenue.FinID = FinancialID.ID AND  Revenue.empID = e. EmpID AND Revenue.customerID = m.ID")->fetchAll(PDO::FETCH_BOTH);
+        $service->pageTitle = 'Financial';
+        $service->revenueList = $revenueList;
+        $service->render('layouts/group11/employee.php');
     });
 
     $klein->respond('GET', '/staff/employee/expense', function ($request, $response, $service) {
--- layouts/group11/component/revenue.php
+++ layouts/group11/component/revenue.php
@@ -24,50 +24,41 @@
             <th>Amount</th>
           </tr>
       </thead>
+      <?php
+        for($i = 1; $i < count($this->list); $i++) {
+      ?>
       <tbody>
           <tr class="clickable" data-toggle="collapse" data-target="#group-of-rows-1"
           aria-expanded="false" aria-controls="group-of-rows-1">
               <td><i class="fas fa-plus"></i></td> <!-- not bind data here-->
-              <td>data</td>
+              <td><?=$this->list[$i]['dName']?></td>
             	<td></td> <!-- not bind data here-->
               <td></td> <!-- not bind data here-->
               <td></td> <!-- not bind data here-->
-              <td>Amount</td>
+              <td><?=$this->list[$i]['total']?></td>
           </tr>
       </tbody>
       <tbody id="group-of-rows-1" class="collapse">
           <tr><!-- this is when collapse bind all data in each department here -->
-              <td>1</td>
-              <td>data 1</td>
-            	<td>data 1</td>
-              <td>data 1</td>
-            	<td>data 1</td>
-              <td>data 1</td>
+              <td><?=$this->list[$i]['dName']?></td>
+              <?php
+                for($j = 0; $j < count($this->revenueList); $j++) {
+                  if ($this->list[$i]['dName'] == $this->revenueList[$j]['dName'] ) {
+              ?>
+                <td><?=$this->revenueList[$j]['transactionId'] ?></td>
+                <td><?=$this->revenueList[$j]['dName'] ?></td>
+                <td><?=$this->revenueList[$j]['date'] ?></td>
+                <td><?=$this->revenueList[$j]['empFN']." ".$this->revenueList[$i]['empLN']?></td>
+                <td><?=$this->revenueList[$j]['memFN']." ".$this->revenueList[$i]['memLN'];?></td>
+                <td><?=$this->revenueList[$j]['amount'] ?></td>
 
+              <?php
+                  }
+              ?>
+              <?php } ?>
           </tr>
       </tbody>
-      <tbody>
-          <tr class="clickable" data-toggle="collapse" data-target="#group-of-rows-2"
-          aria-expanded="false" aria-controls="group-of-rows-2">
-
-              <td><i class="fas fa-plus"></i></td> <!-- not bind data here-->
-              <td>data</td>
-              <td></td> <!-- not bind data here-->
-              <td></td> <!-- not bind data here-->
-              <td></td> <!-- not bind data here-->
-              <td>Amount</td>
-          </tr>
-      </tbody>
-      <tbody id="group-of-rows-2" class="collapse">
-          <tr>
-            <td>1</td>
-            <td>data 1</td>
-            <td>data 1</td>
-            <td>data 1</td>
-            <td>data 1</td>
-            <td>data 1</td>
-          </tr>
-      </tbody>
+      <?php } ?>
   </table>
 </div>
 

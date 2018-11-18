15bad5bc73767a343bd76825e2c5636793c5e9c1
aofpcc <Bunyarit65@hotmail.co.th>
2018-11-18 22:28:26 +0700

    [Disable] Email echo

---
+++ layouts/group2/ticket.php
@@ -0,0 +1,95 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title><?=$this->pageTitle ?></title>
+
+    <!-- Web-Fonts -->
+    	<link href='//fonts.googleapis.com/css?family=Raleway:400,500,600,700,300' rel='stylesheet' type='text/css'>
+    	<link href='//fonts.googleapis.com/css?family=Open+Sans:400,600,700' rel='stylesheet' type='text/css'>
+    <!-- //Web-Fonts -->
+
+    	<link rel="stylesheet" href="/layouts/group2/css/ticket/style.css" type="text/css" media="all" />
+  </head>
+  <body>
+
+	<!-- Content-Starts-Here -->
+	<div class="content">
+
+	<h1>BOOKING</h1>
+
+		<div class="container">
+
+			<h2>HOMESTAY</h2>
+
+			<div class="product">
+				<div class="product-image">
+					<div class="img-resize"><img src="/layouts/group2/images/ticket/mainmovie/homestay.jpg"></div>
+				</div>
+				<div class="product-info">
+					<p><span>Movie</span>Homestay</p>
+					<p><span>Showtime</span>14:20</p>
+					<p><span>Date</span>25 Oct 2018</p>
+					<p><span>Theatre</span> 8 </p>
+					<p><span>Seat No.</span> E4,E5,E6 </p>
+					<p><span>Total </span> 360 THB</p>
+
+					<div class="cart"><a href="#">Submit and Out</a></div>
+				</div>
+				<div class="clear"></div>
+			</div>
+
+			<div class="accordion">
+				<section class="ac-container">
+					<div>
+						<input id="ac-1" name="accordion-1" type="checkbox" />
+						<label for="ac-1">Description</label>
+						<article class="ac-small" style="overflow-y:scroll">
+							<p>เมื่อ "ร่างชั่วคราว" คือรางวัลสุดมหัศจรรย์จากสวรรค์เขามีเวลา 100 วัน เพื่อเปลี่ยนรางวัลเป็นชีวิต..."มึงได้รางวัลนะ"ผู้ชายท่าทางลึกลับที่เรียกตัวเองว่า ผู้คุม (นพชัย ชัยนาม) บอกผม ในขณะที่เรายืนประจันหน้ากันบนผนังตึกของโรงพยาบาลที่หมุนพลิกราวกับแรงโน้มถ่วงกลับด้าน! ผู้คุมไม่รอให้ผมปะติดปะต่อเรื่องราวในหัว เขากระชากคอเสื้อผมให้มาฟังคำอธิบายถึงรางวัลที่วิญญาณเร่ร่อนอย่างผมได้รับ นั่นก็คือการได้กลับมามีชีวิตอีกครั้งในร่างของเด็กม.ปลายที่ชื่อ มิน (ธีรดนย์ ศุภพันธุ์ภิญโญ) ที่นอนนิ่งอยู่ในตู้เก็บศพของโรงพยาบาลแห่งนี้ จะว่าไปการได้มาอยู่ในร่างใหม่ ก็ไม่ต่างอะไรกับการอยู่โฮมสเตย์ คืออยู่ได้แค่ชั่วคราว แถมยังไม่ได้อยู่ฟรีๆ เพราะผมต้องหาคำตอบให้ได้ภายใน 100 วัน ว่า "มินตายเพราะใคร" ถ้าตอบไม่ได้ ผมจะต้องตายและจากร่างโฮมสเตย์นี้ไปตลอดกาลเอาเข้าจริงๆ ผมก็ไม่คิดว่าจะอินกับการอยู่ในร่างโฮมสเตย์นี้ซักเท่าไหร่ การมีครอบครัวใหม่ มีเพื่อนใหม่ ก็ไม่ทำให้หัวใจเต้นแรงเท่าการได้มีความรักครั้งใหม่ ผมได้รู้จักกับ พาย (เฌอปราง อารีย์กุล) พี่รหัสของมิน ผู้หญิงที่ทำให้ผมอยากอยู่ในร่างโฮมสเตย์นี้ตลอดไป แต่เวลา ชีวิต และความรัก เป็นเหมือนรางวัลที่สวรรค์ให้ผมมาแค่ชั่วคราว ผมจึงต้องทำทุกวิถีทางเพื่อตอบคำถามผู้คุมให้ได้ว่า “มินตายเพราะใคร” ก่อนที่เวลาชีวิตในร่างโฮมสเตย์ของผมจะหมดลง....</p>
+						</article>
+					</div>
+        <div>
+						<input id="ac-2" name="accordion-1" type="checkbox" />
+						<label for="ac-2">More Movie</label>
+						<article class="ac-large ac-image" style="overflow-y:scroll">
+							<div class="accordion-image">
+								<div class="ac-img">
+                  <!-- <div class="container"></div> -->
+                  <h3>Overlord</h3>
+									<img src="/layouts/group2/images/ticket/moremovie/overlord.jpg">
+								</div>
+								<div class="ac-img" >
+                  <h3>The Grinch</h3>
+                  <img src="/layouts/group2/images/ticket/moremovie/thegrin.jpg">
+								</div>
+								<div class="clear"></div>
+							</div>
+						</article>
+					</div>
+					<div>
+						<input id="ac-3" name="accordion-1" type="checkbox" />
+						<label for="ac-3">Rating & Reviews</label>
+						<article class="ac-large ac-review">
+							<h3>"Excellent Fragrance!"</h3>
+							<h4>Reno Jean</h4>
+							<p>GDH ไม่ทำให้ผิดหวังจริงๆ นักแสดงเล่นดี กราฟฟิกก็สวยอลังใช่เล่น ชอบมากๆ</p>
+							<span>5 Stars</span>
+							<a href="#" class="next">Next Review &rarr;</a>
+						</article>
+					</div>
+					<div>
+
+					</div>
+				</section>
+			</div>
+
+		</div>
+
+	</div>
+
+</body>
+</html>

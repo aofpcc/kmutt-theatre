9104f774011509f2559d40762a320f12bbc0f55c
aofpcc <Bunyarit65@hotmail.co.th>
2018-11-18 22:28:26 +0700

    [Disable] Email echo

--- config/login.php
+++ config/login.php
@@ -7,7 +7,7 @@ class MailSender
         $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
         try {
             //Server settings
-      $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+      $mail->SMTPDebug = 0;                                 // Enable verbose debug output
       $mail->isSMTP();                                      // Set mailer to use SMTP
       $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
       $mail->SMTPAuth = true;                               // Enable SMTP authentication

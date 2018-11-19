f637a5b74549ac8ce0e99f8653e41b5169485596
narukid <naruhpak.210@mail.kmutt.ac.th>
2018-11-19 14:58:06 +0700

    edit file1
    round2

--- groups/group5/file1.php
+++ groups/group5/file1.php
@@ -4,82 +4,13 @@ $klein->respond('GET', '/membership', function ($request, $response, $service) {
   global $database;
   $conn = $database->getConnection();
 
-<<<<<<< HEAD
-  $service->render('layouts/group5/membership.php');
-});
-=======
-<<<<<<< HEAD
-=======
-  $query = "SELECT * from movies";
-  $stmt = $conn->prepare($query);
-  $stmt->execute();
->>>>>>> web_customer
-
-// chage username
-$klein->respond('GET', '/change/username', function ($request, $response, $service) {
-  $service->render('layouts/group5/changeUsername.php');
-});
-
-<<<<<<< HEAD
-// change password
-$klein->respond('GET', '/change/password', function ($request, $response, $service) {
-  $service->render('layouts/group5/changePassword.php');
-});
-
-// Change email
-$klein->respond('GET', '/change/email', function ($request, $response, $service) {
-  $service->render('layouts/group5/changeEmail.php');
-});
-
-// Change phone number
-$klein->respond('GET', '/change/phonenumber', function ($request, $response, $service) {
-  $service->render('layouts/group5/changePhoneNumber.php');
-});
-
-// chage username
-$klein->respond('GET', '/change/username', function ($request, $response, $service) {
-  $service->render('layouts/group5/changeUsername.php');
-});
-
-// change password
-$klein->respond('GET', '/change/password', function ($request, $response, $service) {
-  $service->render('layouts/group5/changePassword.php');
-});
-
-=======
-  $service->allMovies = $arr;
-  $service->pageTitle = 'Fish and Chips';
->>>>>>> web_customer
   $service->render('layouts/group5/membership.php');
 });
 
 // chage username
-$klein->respond('GET', '/change/username', function ($request, $response, $service) {
-  $service->render('layouts/group5/changeUsername.php');
-});
-
-// change password
-$klein->respond('GET', '/change/password', function ($request, $response, $service) {
-  $service->render('layouts/group5/changePassword.php');
-});
-
->>>>>>> web_customer
-// Change email
-$klein->respond('GET', '/change/email', function ($request, $response, $service) {
-  $service->render('layouts/group5/changeEmail.php');
-});
-
-// Change phone number
-$klein->respond('GET', '/change/phonenumber', function ($request, $response, $service) {
-  $service->render('layouts/group5/changePhoneNumber.php');
-<<<<<<< HEAD
-=======
-});
-
-// chage username
-$klein->respond('GET', '/change/username', function ($request, $response, $service) {
-  $service->render('layouts/group5/changeUsername.php');
-});
+// $klein->respond('GET', '/change/username', function ($request, $response, $service) {
+//   $service->render('layouts/group5/changeUsername.php');
+// });
 
 // change password
 $klein->respond('GET', '/change/password', function ($request, $response, $service) {
@@ -94,5 +25,9 @@ $klein->respond('GET', '/change/email', function ($request, $response, $service)
 // Change phone number
 $klein->respond('GET', '/change/phonenumber', function ($request, $response, $service) {
   $service->render('layouts/group5/changePhoneNumber.php');
->>>>>>> web_customer
+});
+
+// Login
+$klein->respond('GET', '/login', function ($request, $response, $service) {
+  $service->render('layouts/group5/login.php');
 });

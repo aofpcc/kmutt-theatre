78d4f5e6461fb65f31d25051ca65adddbce8a481
Chynnakryt M <chynury@icloud.com>
2018-11-19 18:10:48 +0700

    เลิกอ่านของคนอื่นซักที

--- layouts/group5/login.php
+++ layouts/group5/login.php
@@ -31,7 +31,7 @@
           </br>
 
         </form>
-        <p class="message2">Don't have an account? <a href="http://localhost:8000/register">Resgister</a></p>
+        <p class="message2">Don't have an account? <a href="/register">Resgister</a></p>
       </div>
     </div>
   </body>

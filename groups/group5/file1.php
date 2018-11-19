479158b615a0d48c905f75cc2a28d64fc93c110d
narukid <naruhpak.210@mail.kmutt.ac.th>
2018-11-19 14:22:22 +0700

    fix change page

--- groups/group5/file1.php
+++ groups/group5/file1.php
@@ -1,5 +1,6 @@
 <?php
-$klein->respond('GET', '/www', function ($request, $response, $service) {
+// member information
+$klein->respond('GET', '/membership', function ($request, $response, $service) {
   global $database;
   $conn = $database->getConnection();
 
@@ -12,7 +13,7 @@ $klein->respond('GET', '/www', function ($request, $response, $service) {
 
   $service->allMovies = $arr;
   $service->pageTitle = 'Fish and Chips';
-  $service->render('layouts/group5/home.php');
+  $service->render('layouts/group5/membership.php');
 });
 
 $klein->respond('GET', '/www2', function ($request, $response, $service) {
@@ -29,4 +30,24 @@ $klein->respond('GET', '/www2', function ($request, $response, $service) {
   $service->allMovies = $arr;
   $service->pageTitle = 'View All Movies';
   $service->render('layouts/group5/home.php');
+});
+
+// chage username
+$klein->respond('GET', '/change/username', function ($request, $response, $service) {
+  $service->render('layouts/group5/changeUsername.php');
+});
+
+// change password
+$klein->respond('GET', '/change/password', function ($request, $response, $service) {
+  $service->render('layouts/group5/changePassword.php');
+});
+
+// Change email
+$klein->respond('GET', '/change/email', function ($request, $response, $service) {
+  $service->render('layouts/group5/changeEmail.php');
+});
+
+// Change phone number
+$klein->respond('GET', '/change/phonenumber', function ($request, $response, $service) {
+  $service->render('layouts/group5/changePhoneNumber.php');
 });

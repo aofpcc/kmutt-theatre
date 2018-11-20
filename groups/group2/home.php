cff949812cfebcfee1588283affac1a26ac172d9
Panusronp <panusronphansod@gmail.com>
2018-11-21 01:29:38 +0700

    try change ticket

--- groups/group2/home.php
+++ groups/group2/home.php
@@ -1,19 +1,23 @@
 <?php
-  // error_reporting(E_ALL);
-  // ini_set('display_errors', 1);
+  error_reporting(E_ALL);
+  ini_set('display_errors', 1);
 
   $klein->respond('GET', '/group2/home', function ($request, $response, $service){
-      global $database;
+  global $database;
   $conn = $database->getConnection();
 
-  $query = "SELECT MemberID from G05_Member_profile";
+  $card_no = $_GET['card_no'];
+
+
+
+  $query = "SELECT MemberID from G05_Member_profile where Card_ID = '$card_no'" ;
   $stmt = $conn->prepare($query);
   $stmt->execute();
 
-  $num = $stmt->rowCount();
-  $arr = $stmt->fetchAll(PDO::FETCH_BOTH);
-
-  $service->allMovies = $arr;
+  // $num = $stmt->rowCount();
+  // $arr = $stmt->fetchAll(PDO::FETCH_BOTH);
+  //
+  // $service->allMovies = $arr;
   $service->pageTitle = 'KMUTT THEATRE | Member';
 
   $service->render('layouts/group2/member.php');

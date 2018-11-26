e689efd3ac9ca9804b4f43df7b4e6710491d5423
Panusronp <panusronphansod@gmail.com>
2018-11-26 23:27:30 +0700

    //

--- groups/group2/home.php
+++ groups/group2/home.php
@@ -24,6 +24,11 @@ $klein->respond('POST', '/group2/check_card_no', function ($request, $response, 
   $conn = $database->getConnection();
 
   $card_no = $request->card_no;
+                // $user = $_GET['user'];
+                // $password = $_GET['pass'];
+                
+                // $query = "SELECT MemberID from G05_Member_profile where Email = '$user' and PhoneNumber = '$password' ";
+                
 
   $query = "SELECT ID_Card from G05_Member_profile where ID_Card = '$card_no'" ;
   $stmt = $conn->prepare($query);

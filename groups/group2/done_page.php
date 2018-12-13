b741062a9397a6ac69f581561e4d2e080579661c
aofpcc <Bunyarit65@hotmail.co.th>
2018-12-13 16:10:07 +0700

    [Edit Conflict File "Done_Page"]

--- groups/group2/done_page.php
+++ groups/group2/done_page.php
@@ -1,12 +1,5 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
 
-<<<<<<< HEAD
-  $klein->respond('GET', '/group2/home_page/select_movie/select_time/select_seat/pay_ment/done', function ($request, $response, $service){
-  global $database;
-  $conn = $database->getConnection();
-=======
 $klein->respond('GET', '/group2/home_page/select_movie/select_time/select_seat/done', function ($request, $response, $service) {
     global $database;
     $conn = $database->getConnection();
@@ -17,7 +10,6 @@ $klein->respond('GET', '/group2/home_page/select_movie/select_time/select_seat/d
     //   $stmt = $conn->prepare($query);
     //     $stmt->execute();
     //     $service->employee = $stmt->fetchAll(PDO::FETCH_BOTH);
->>>>>>> web_employee
 
     // $query = "SELECT seat_no from G02_Ticket_history";
     // $stmt = $conn->prepare($query);

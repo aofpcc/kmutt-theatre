a22061933d3fb0530c660e465a3abf75311a73c0
Kornkanok Sangwichien <mu.nsnun@gmail.com>
2018-12-15 19:47:40 +0700

    [Bank] reciept detail

--- layouts/core/bank.php
+++ layouts/core/bank.php
@@ -163,12 +163,12 @@
               <p>Chekout</p>
             </a></li>
 
-            <li role="presentation"><a href="#payment" aria-controls="payment" role="tab" data-toggle="tab"><i class="fa fa-check" aria-hidden="true"></i>
+            <li role="presentation"><a href="#payment" aria-controls="payment" role="tab" data-toggle="tab"><i class="fa fa-file" aria-hidden="true"></i>
               <p>Confirm</p>
             </a></li>
 
-            <li role="presentation"><a href="#invoice" aria-controls="invoice" role="tab" data-toggle="tab"><i class="fa fa-file" aria-hidden="true"></i>
-              <p>Invoice</p>
+            <li role="presentation"><a href="#finish" aria-controls="finish" role="tab" data-toggle="tab"><i class="fa fa-check" aria-hidden="true"></i>
+              <p>Finish</p>
             </a></li>
           </ul>
         </center>
@@ -235,7 +235,7 @@
 
                   <div class="row">
                     <div class="col-xs-12">
-                      <button class="subscribe btn btn-success btn-lg btn-block" type="button">Pay</button>
+                      <button class="subscribe btn btn-success btn-lg btn-block" type="button">Next</button>
                     </div>
                   </div>
                   <div class="row" style="display:none;">
@@ -255,11 +255,105 @@
           <div role="tabpanel" class="tab-pane" id="payment">
             <div class="design-process-content">
               <h3 class="semi-bold">Confirm</h3>
-              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincid unt ut laoreet dolore magna aliquam erat volutpat</p>
+              <!-- confirm -->
+
+              <div class="container">
+                  <div class="row">
+                      <div class="well col-xs-10 col-sm-10 col-md-6 col-xs-offset-1 col-sm-offset-1 col-md-offset-3">
+                          <div class="row">
+                              <div class="col-xs-6 col-sm-6 col-md-6">
+                                  <address>
+                                      <strong>Elf Cafe</strong>
+                                      <br>
+                                      2135 Sunset Blvd
+                                      <br>
+                                      Los Angeles, CA 90026
+                                      <br>
+                                      <abbr title="Phone">P:</abbr> (213) 484-6829
+                                  </address>
+                              </div>
+                              <div class="col-xs-6 col-sm-6 col-md-6 text-right">
+                                  <p>
+                                      <em>Date: 1st November, 2013</em>
+                                  </p>
+                                  <p>
+                                      <em>Receipt #: 34522677W</em>
+                                  </p>
+                              </div>
+                          </div>
+                          <div class="row">
+                              <div class="text-center">
+                                  <h1>Receipt</h1>
+                              </div>
+                              </span>
+                              <table class="table table-hover">
+                                  <thead>
+                                      <tr>
+                                          <th>Product</th>
+                                          <th>#</th>
+                                          <th class="text-center">Price</th>
+                                          <th class="text-center">Total</th>
+                                      </tr>
+                                  </thead>
+                                  <tbody>
+                                      <tr>
+                                          <td class="col-md-9"><em>Baked Rodopa Sheep Feta</em></h4></td>
+                                          <td class="col-md-1" style="text-align: center"> 2 </td>
+                                          <td class="col-md-1 text-center">$13</td>
+                                          <td class="col-md-1 text-center">$26</td>
+                                      </tr>
+                                      <tr>
+                                          <td class="col-md-9"><em>Lebanese Cabbage Salad</em></h4></td>
+                                          <td class="col-md-1" style="text-align: center"> 1 </td>
+                                          <td class="col-md-1 text-center">$8</td>
+                                          <td class="col-md-1 text-center">$8</td>
+                                      </tr>
+                                      <tr>
+                                          <td class="col-md-9"><em>Baked Tart with Thyme and Garlic</em></h4></td>
+                                          <td class="col-md-1" style="text-align: center"> 3 </td>
+                                          <td class="col-md-1 text-center">$16</td>
+                                          <td class="col-md-1 text-center">$48</td>
+                                      </tr>
+                                      <tr>
+                                          <td>   </td>
+                                          <td>   </td>
+                                          <td class="text-right">
+                                          <p>
+                                              <strong>Subtotal: </strong>
+                                          </p>
+                                          <p>
+                                              <strong>Tax: </strong>
+                                          </p></td>
+                                          <td class="text-center">
+                                          <p>
+                                              <strong>$6.94</strong>
+                                          </p>
+                                          <p>
+                                              <strong>$6.94</strong>
+                                          </p></td>
+                                      </tr>
+                                      <tr>
+                                          <td>   </td>
+                                          <td>   </td>
+                                          <td class="text-right"><h4><strong>Total: </strong></h4></td>
+                                          <td class="text-center text-danger"><h4><strong>$31.53</strong></h4></td>
+                                      </tr>
+                                  </tbody>
+                              </table>
+                              <button type="button" class="btn btn-success btn-lg btn-block">
+                                  Pay Now   <span class="glyphicon glyphicon-chevron-right"></span>
+                              </button></td>
+                          </div>
+                      </div>
+                  </div>
+                </div>
+
+
+
             </div>
           </div>
 
-          <div role="tabpanel" class="tab-pane" id="invoice">
+          <div role="tabpanel" class="tab-pane" id="finish">
             <div class="design-process-content">
               <h3>Invoice</h3>
               <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincid unt ut laoreet dolore magna aliquam erat volutpat. </p>

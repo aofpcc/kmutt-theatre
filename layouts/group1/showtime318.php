722f2e645cc8decddda10ebba0b362578e177de5
aofpcc <Bunyarit65@hotmail.co.th>
2018-12-17 10:45:42 +0700

    [Edit Group 1] Show movie showtime per each

--- layouts/group1/showtime318.php
+++ layouts/group1/showtime318.php
@@ -25,6 +25,7 @@
 
   <!-- Custom styles for this template -->
   <link href="css/1-col-portfolio.css" rel="stylesheet">
+  <link rel="stylesheet" href="/layouts/group1/css/showtime.css">
 
 </head>
 
@@ -38,6 +39,7 @@
 
   <div class="card card-temp shadow-lg">
     <div class="container">
+
       <br>
       <div class="row">
         <div class="col-lg-4 col-md-6 mb-4">
@@ -46,7 +48,6 @@
                 alt=""></a>
           </div>
         </div>
-
         <div class="col-md-8">
           <br><br><br><br>
           <h4>Fantastic Beasts The Crimes of Grindelwald</h4>
@@ -61,59 +62,21 @@
   <br><br>
   <hr>
 
-  <nav class="nav nav-pills nav-fill">
-    <a class="nav-item nav-link active" href="#">Thu<br><small>15 Nov 2018</small></a>
+  <nav class="nav nav-pills nav-fill" id="movie-links">
+    <!-- <a class="nav-item nav-link active" href="#">Thu<br><small>15 Nov 2018</small></a>
     <a class="nav-item nav-link" href="#">Fri<br><small>16 Nov 2018</small></a>
     <a class="nav-item nav-link" href="#">Sat<br><small>17 Nov 2018</small></a>
     <a class="nav-item nav-link disabled" href="#">Mon<br><small>18 Nov 2018</small></a>
-    <a class="nav-item nav-link disabled" href="#">Tue<br><small>19 Nov 2018</small></a>
+    <a class="nav-item nav-link disabled" href="#">Tue<br><small>19 Nov 2018</small></a> -->
+    <?php foreach($this->query as $q) { ?>
+    <a class="nav-item nav-link <?=$q["status"]?>" href="/customer/movies/showtime/all/<?=$this->movie_id?>/<?=$q["value"]?>"><?=$q["date"] ?><br><small><?=$q["str"] ?></small></a>
+    <?php } ?>
   </nav>
   <hr>
   <!-- Project One -->
-  <div class="card card-temp shadow-sm">
-    <div class="row">
-      <div class="col-lg-4 col-md-6 mb-4">
-        <a href="#">
-          <br><br>
-          <center><img class="img-fluid rounded mb-3 mb-md-0" src="https://lh3.googleusercontent.com/1fVPsPOjLnZ3tNzeLrlcr-QKvV_9DUnRgEPXYWqkcmRpr6EPjAjanHquRzgLQZPJG9V78tzQ6sfL08Ma4S9W=h59"
-              alt=""></center>
-        </a>
-      </div>
-      <div class="col-md-5">
-        <br>
-        <h4>ENG</h4>
-        <a class="btn btn-primary active" href="#">10:20</a>
-        <a class="btn btn-outline-primary" href="#">12:20</a>
-        <a class="btn btn-outline-primary" href="#">15:20</a>
-        <a class="btn btn-outline-primary" href="#">17:20</a>
-        <a class="btn btn-outline-primary" href="#">21:20</a>
-      </div>
-    </div>
-    <!-- /.row -->
-
-    <hr>
-
-    <!-- Project Two -->
-    <div class="row">
-      <div class="col-lg-4 col-md-6 mb-45">
-        <br><br>
-        <h3>
-          <center>Digital Cinema</center>
-        </h3>
-        <br><br>
-      </div>
-
-      <div class="col-md-5">
-        <br>
-        <h4>TH</h4>
-        <a class="btn btn-primary active" href="#">15:00</a>
-        <a class="btn btn-outline-primary" href="#">17:45</a>
-        <a class="btn btn-outline-primary" href="#">21:15</a>
-
-      </div>
-    </div>
-    <!-- /.row -->
-  </div>
+  <!-- OverHere -->
+  <div id="showtime"></div>
+    
 </div>
 
 <br><br><br>
@@ -122,3 +85,34 @@
 <!-- Bootstrap core JavaScript -->
 <script src="vendor/jquery/jquery.min.js"></script>
 <!-- <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script> -->
+<script>
+  $(document).ready(function(){
+    var movie_id = "<?=$this->movie_id?>";
+    var date = "<?=$this->datenow?>";
+    $.get("/customer/movies/showtime/all/"+movie_id+"/"+date).done(function(data){
+      $("#showtime").html(data);
+    });
+  });
+
+  function openLink(e){
+    $.get(e).done(function(data){
+      $("#showtime").html(data);
+    });
+  }
+
+  function newActive(e){
+    console.log(e);
+    $.each( $("#movie-links > a"), function(k, v) {
+      $(v).removeClass('active');
+    });
+    $(e).addClass('active');
+  }
+
+  $("#movie-links > a").click(function(e) {
+    // Prevent a page reload when a link is pressed
+    e.preventDefault();
+    // Call the scroll function
+    openLink(this.href);
+    newActive(this);
+});
+</script>

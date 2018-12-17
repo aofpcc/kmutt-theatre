4e9b80f5445fe8c4577319542af931e7cd6d6ac5
Sarawut Rattanawichein <martdekza@hotmail.com>
2018-12-17 20:43:21 +0700

    Group 8 upload pic

--- layouts/group8/searchpromotion.php
+++ layouts/group8/searchpromotion.php
@@ -1,10 +1,10 @@
 <br>
 <h2>Edit Promotion</h1>
-<form action="/emp/promotion/edit2" method="post">
+<form action="/emp/promotion/edit" method="post">
 
  <div class="form-group">
     <label for="" class="form-label">Promotion name</label>
-    <input name="promotion_name" type="text" class="form-control" placeholder="xxxx">
+    <input name="PromoNameEdit" type="text" class="form-control" placeholder="xxxx">
   </div>
   <div class="form-group">
   <button class="btn btn-primary" type="submit">Search</button>

7ef5b3f55de5301dd1235e33465b0450a4467af1
freshthit <fresh.thit@me.com>
2018-12-18 17:05:37 +0700

    [MOBILE - BOOKING] change back the movie table name

--- groups/group12/file1.php
+++ groups/group12/file1.php
@@ -19,7 +19,7 @@ $klein->respond('GET', '/mobile/getShowtime', function ($request, $response, $se
   global $database;
   $conn = $database->getConnection();
 
-  $query = "SELECT * from G09_Movie_Name";
+  $query = "SELECT * from G09_Movie";
   $stmt = $conn->prepare($query);
   $stmt->execute();
 

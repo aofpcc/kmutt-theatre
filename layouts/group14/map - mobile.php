715dc0ea95d4cb221b27b16d2fa2a01330cda2bf
Nuttoomuch <nutbpm@gmail.com>
2018-12-19 04:44:55 +0700

    asdfasdf

--- layouts/group14/map - mobile.php
+++ layouts/group14/map - mobile.php
@@ -31,7 +31,7 @@
     <div id="map" style=" height: 50%; width: 100%;"></div>
     <hr style="height:2pt; visibility:hidden; margin-bottom:-1px; margin-top:3px" />
     <div action="/group14/map/action" method="post">
-        <div class=""><input class="" type="text" id="myInput" onkeyup="myFunction()" placeholder="  Search location..." style="width: 100%; height: 42px; border: none; border-radius: 6px; font-size: 17px; "/></div>
+        <div class=""><input class="" type="text" id="myInput" onkeyup="myFunction()" placeholder="  Search location..." style="width: 100%; height: 40px; border: none; border-radius: 6px; font-size: 17px; "/></div>
         <hr style="height:2pt; visibility:hidden; margin-bottom:-1px; margin-top:3px" />
         <div class="scrollable scrollbar-danger" style='height: 25%'>
             <div class="force-overflow" id="BtnContainer">

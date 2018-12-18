d9f695e17249860264e5c494f9b910927b867a1f
Nuttoomuch <nutbpm@gmail.com>
2018-12-18 22:03:11 +0700

    gg

--- layouts/group14/map - mobile.php
+++ layouts/group14/map - mobile.php
@@ -17,6 +17,8 @@
           width: 100%;
         }
         
+        
+        
 
     </style>
     <meta charset="utf-8">
@@ -33,7 +35,7 @@
     <div id="map" style="margin-top: 8px; height: 50%; width: 100%;"></div>
     <hr style="height:2pt; visibility:hidden; margin-bottom:-1px; margin-top:3px" />
     <form action="/group14/map/action" method="post">
-        <div class="input-group stylish-input-group"><input class="form-control" type="text" id="myInput" onkeyup="myFunction()" placeholder="Search location..." style="width: 100%; height:4%"/></div>
+        <div class=""><input class="" type="text" id="myInput" onkeyup="myFunction()" placeholder="Search location..." style="width: 100%; border: none; border-bottom: 2px solid red;"/></div>
         <hr style="height:2pt; visibility:hidden; margin-bottom:-1px; margin-top:3px" />
         <div class="scrollable scrollbar-danger" style='height: 22%'>
             <div class="force-overflow" id="BtnContainer">

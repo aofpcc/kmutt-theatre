65475b4c811110ded0a6fb553188bf08201f6fdc
Gunt Chanmas <gunt4826@outlook.com>
2018-12-18 12:03:27 +0700

    [Edit] group 10

--- layouts/group10/getdata.php
+++ layouts/group10/getdata.php
@@ -4,12 +4,15 @@
 
   $id = $_POST['id'];
 
-  $stmt = $conn->prepare("SELECT * FROM advertisement WHERE id = :id");
+  // $stmt = $conn->prepare("SELECT * FROM advertisement WHERE id = :id");
+  $stmt = $conn->prepare("SELECT i.id, name, url, banner, start_date, end_date, status
+                          FROM G10_Advertisement_info AS i, G10_Advertisement_link AS l,
+                           G10_Advertisement_banner AS b, G10_Advertisement_time AS t
+                          WHERE i.id = l.id AND l.id = b.id AND b.id = t.id AND i.id = :id");
   $stmt->bindParam(':id', $id);
   $stmt->execute();
   $data = $stmt->fetch(PDO::FETCH_ASSOC);
 
-  // echo $data;
   echo json_encode($data);
 
 ?>

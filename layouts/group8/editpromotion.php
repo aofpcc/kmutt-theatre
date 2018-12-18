af64d84f4188084eb596364dfb01852281319a3c
Sarawut Rattanawichein <martdekza@hotmail.com>
2018-12-18 11:38:53 +0700

    Auto stash before merge of "web_employee" and "origin/web_employee"

--- layouts/group8/editpromotion.php
+++ layouts/group8/editpromotion.php
@@ -2,33 +2,29 @@
 <h2>Edit Promotion</h1>
 <form action="/emp/promotion/edit" method="post" enctype="multipart/form-data">
 
- <div class="form-group">
-    <label for="" class="form-label">Promotion Name</label>
-    <input name="promotion_name" type="text" class="form-control" placeholder="xxxx">
-  </div>
     <div class="form-group">
     <label for="" class="form-label">Promotion Name</label>
-    <input name="promotion_name" type="text" class="form-control" placeholder="xxxx">
+    <input name="promotion_name" type="text" class="form-control" value = "<?=$promotion["PromoName"] ?>">
   </div>
 
   <div class="form-group">
     <label for="" class="form-label">Description</label>
-    <input name="promotion_des" type="text" class="form-control" placeholder="xxxx">
+    <input name="promotion_des" type="text" class="form-control" value ="<?=$promotion["Description"] ?>">
   </div>
 
   <div class="form-group">
     <label for="" class="form-label">Discount</label>
-    <input name="promotion_dis" type="text" class="form-control" placeholder="xxxx">
+    <input name="promotion_dis" type="text" class="form-control"value = "<?=$promotion["Discount"] ?>">
   </div>
 
   <div class="form-group">
     <label for="" class="form-label">Start Date</label>
-    <input name="promotion_start" type="date" class="form-control" placeholder="dd/mm/yy">
+    <input name="promotion_start" type="date" class="form-control"value ="<?=$promotion["StartDate"] ?>">
   </div>
 
   <div class="form-group">
     <label for="" class="form-label">End Date</label>
-    <input name="promotion_end" type="date" class="form-control" placeholder="dd/mm/yy">
+    <input name="promotion_end" type="date" class="form-control"value = "<?=$promotion["EndDate"] ?>">
   </div>
 
   <div class="form-group">

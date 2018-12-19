1edf06463445f7baefd9d920a885707103407195
Panusronp <panusronphansod@gmail.com>
2018-12-19 18:21:20 +0700

    final

--- groups/group2/new_dividepoint.php
+++
@@ -1,8 +0,0 @@
-<?php
-$klein->respond('GET', '/ticket/[:branch_id]', function($request, $response, $service, $app, $validator){
-    global $database;
-    $conn = $database->getConnection();
-
-
-
-});

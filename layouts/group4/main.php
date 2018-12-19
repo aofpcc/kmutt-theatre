6f68030d15211a1b0855061f81ac33c51426df62
Unknown <thanna753@gmail.com>
2018-12-19 17:23:04 +0700

    [G4] edit g04 main

--- layouts/group4/main.php
+++ layouts/group4/main.php
@@ -1,11 +1,35 @@
-<div class="container">
+<!-- <div class="container">
 <a href="/emp/showRoomtype">Room type</a><br>
 <a href="/emp/showAmountseat">Amount Seat</a><br>
 <a href="/emp/showRoominfo">Room Info</a><br>
 <a href="/emp/showSeattype">seat type</a><br>
 <a href="/emp/showtime">show time</a><br>
-<a href="/emp/add_showtime">ADD showtime</a><br>
-<a href="/emp/add_roominfo">ADD roominfo</a><br>
-<a href="/emp/add_amountseat">ADD amountseat</a><br>
-<a href="/emp/add_seattype">Add Seat type</a><br>
-</div>
+</div> -->
+<div class="card card-temp shadow-lg">
+      <div class="container">
+        <br>
+         <!-- Contact Section -->
+         <section id="add">
+            <div class="container">
+              <h2 class="text-center text-uppercase text-secondary mb-0">Management Room Seat and Showtime</h2>
+              <hr class="star-dark mb-5">
+              <div class="row">
+                <div class="col-lg-8 mx-auto">
+                  <!-- action form -->
+                  <form>
+                    <div class="form-group">
+                    <div class="container">
+                            <a href="/emp/showRoominfo">Room Info</a><br>
+                            <a href="/emp/showAmountseat">Amount Seat</a><br>
+                            <a href="/emp/showtime">show time</a><br>
+                      <br><br>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </section>
+              </div>
+            </div>
+            <br><br>
+          </div>

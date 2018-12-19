d44d0b40517e7fe93d44f40b525c64c05bb35d5d
tangpnm <tang_6528@hotmail.com>
2018-12-19 17:50:12 +0700

    css in history

--- groups/group1/ticket_history_page.php
+++ groups/group1/ticket_history_page.php
@@ -4,7 +4,7 @@ $klein->respond('GET', '/kmutt/ticket/ticket_history', function ($request, $resp
   $service->bootstrap3 = false;
   $conn = $database->getConnection();
 
-  $ticket_id = $conn->query("select seat_ticket, room_id, d.title as name
+  $ticket_id = $conn->query("select seat_ticket, room_id, d.title as name, code
   from G02_Ticket_member a, G02_Ticket_history b, G04_MSRnB_showingroom c, G09_Movie d
   where a.ticket_id = b.id and c.id = b.showtime_id and c.movie_id = d.id and a.userID = $userID;")
   ->fetchAll(PDO::FETCH_ASSOC);
